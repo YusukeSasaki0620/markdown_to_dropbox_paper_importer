4cb1c51e6351b8fd72c25ebd18b43110a9400a35
Yusuke Sasaki <yusuke.sasaki@playground.live>
2021-07-10 08:04:08 +0900

    feat: make dir

--- test-api.rb
+++ test-api.rb
@@ -1,6 +1,28 @@
 require 'dotenv/load'
 require 'dropbox_api'
+require "fileutils"
 
 client = DropboxApi::Client.new
-binding.irb
-client.create_folder("/hoge")
+INPUT_DIR = "input_files/"
+
+# begin
+#   client.create_folder("/hoge")
+# rescue DropboxApi::Errors::FolderConflictError => exception
+#   pp 'folder exsist. continue.'
+# end
+
+
+Dir.chdir(INPUT_DIR)
+Dir.glob("**/*") do |item|
+  pp "find [ #{item} ]"
+  if FileTest.directory?(item)
+    begin
+      client.create_folder("/#{item}")
+      pp "folder [ #{item} ] create."
+    rescue DropboxApi::Errors::FolderConflictError => exception
+      pp "folder [ #{item} ] exsist. continue."
+    end
+  else
+
+  end
+end
