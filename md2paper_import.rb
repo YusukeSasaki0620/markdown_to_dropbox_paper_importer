7e95500158758065d3ae6b82be6dc41b73706340
Yusuke Sasaki <yusuke.sasaki@playground.live>
2021-07-16 22:54:03 +0900

    feat: catch e and error log

--- md2paper_import.rb
+++ md2paper_import.rb
@@ -1,11 +1,15 @@
 require 'dotenv/load'
 require 'dropbox_api'
 require "fileutils"
+require 'csv'
 
 client = DropboxApi::Client.new
 INPUT_DIR = "input_files/"
 PAPER_EXT = ".paper"
-
+FileUtils.mkdir_p('logs')
+CSV.open('logs/errorlog.csv', 'w', encoding: 'UTF-8') do |log_csv|
+  log_csv << ['text', 'exception']
+end
 Dir.chdir(INPUT_DIR)
 Dir.glob("**/*") do |item|
   pp "find [ #{item} ]"
@@ -19,8 +23,15 @@ Dir.glob("**/*") do |item|
   else
     peper_path = Pathname(item).sub_ext(PAPER_EXT)
     pp "create #{peper_path}"
-    File.open(item) do |file|
-      client.paper_create("/#{peper_path}", file, import_format: :markdown )
+    begin
+      raise Error
+      File.open(item) do |file|
+        client.paper_create("/#{peper_path}", file, import_format: :markdown )
+      end
+    rescue => exception
+      CSV.open('../logs/errorlog.csv', 'a', encoding: 'UTF-8') do |log_csv|
+        log_csv << ["create #{peper_path}", exception.to_s]
+      end
     end
   end
 end
